07f2bd4c55ce145b3ee14b977df648541cea74c2
Administrator <948172275@qq.com>
2019-12-21 15:34:49 +0800

    增加套房费用和小房间信息

--- yafy-core/src/main/java/com/yykj/business/dao/AgreementMapper.java
+++ yafy-core/src/main/java/com/yykj/business/dao/AgreementMapper.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public interface AgreementMapper extends MyMapper<Agreement> {
     /**
-     * description: TODO 合同记录
+     * description: 合同记录
      * create by: qhw
      * create time: 2019/11/17 0017 下午 22:52
      */
